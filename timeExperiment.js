4d2e27c00b59d62553f7b96c05ff5c21546b32c9
pranaygandhi1993@live.com <pranaygandhi1993@live.com>
2016-01-18 16:29:40 +0530

    folder restructuring

--- timeExperiment.js
+++
@@ -1,7 +0,0 @@
-var moment = require('moment');
-console.log("*******************************");
-var endDate = moment('Tue Jan 5 23:46:05 2016 -0500', 'ddd mmm D HH:mm:ss');
-var startDate = moment('Sat Dec 26 20:54:01 2015 -0500', 'ddd mmm D HH:mm:ss');
-console.log(endDate);
-var secondsDiff = endDate.diff(startDate, 'seconds');
-console.log(secondsDiff);
