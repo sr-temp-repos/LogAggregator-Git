0cd4f14fb93c038cb29b643e634967402d9e7a4a
pranaygandhi1993@live.com <pranaygandhi1993@live.com>
2016-02-26 02:51:20 +0530

    test commit

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-
-router.get('/', function(req, res) {
-  res.render('contributors', { title: 'Contributors Graph' } );
-});
-
-module.exports = router;
